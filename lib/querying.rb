c3cc3a2b0359493a4b5813c981c7113a1453f5b3
David Hutchison <dhutchis68516@gmail.com>
2020-06-04 18:13:53 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -2,11 +2,13 @@ def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, books.year
   FROM books
   INNER JOIN series ON books.series_id = series.id
-  GROUP BY books.year HAVING series.id = 1"
+  GROUP BY books.year HAVING series.id = 1;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT character.name, characters.motto
+  FROM characters
+  ORDER BY LENGTH(characters.motto) DESC LIMIT 1;"
 end
 
 
